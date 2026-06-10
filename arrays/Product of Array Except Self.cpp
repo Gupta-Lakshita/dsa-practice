14fbacdcc0e14f490a28c3acf7ca6b681f01b1e9
Lakshita Gupta <me.lakshitag@gmail.com>
2026-06-10 23:42:49 +0530

    container most water

--- arrays/Product of Array Except Self.cpp
+++ arrays/Product of Array Except Self.cpp
@@ -2,24 +2,25 @@
 //approach:
 //o(n) space: prefix vector, suffic vector, ans vector
 
-// int n=nums.size();
-// vector<int>ans(n,1);
-// vector<int>prefix;
-// prefix[0]=1;
-// for(int i=0;i<n;i++){
-//     prefix[i]=prefix[i-1]*nums[i-1];
-// }
-// vector<int>suffix;
-// suffix[n-1]=1;
-// for(int i=n-2;i>=0;i--){
-//     suffix[i]=suffix[i+1]*nums[i+1];
-// }
-// for (int i=0;i<n;i++){
-//     ans[i]=prefix[i]*suffix[i];
-// }
-// return ans;
+int n=nums.size();
+vector<int>ans(n,1);
+vector<int>prefix;
+prefix[0]=1;
+for(int i=0;i<n;i++){
+    prefix[i]=prefix[i-1]*nums[i-1];
+}
+vector<int>suffix;
+suffix[n-1]=1;
+for(int i=n-2;i>=0;i--){
+    suffix[i]=suffix[i+1]*nums[i+1];
+}
+for (int i=0;i<n;i++){
+    ans[i]=prefix[i]*suffix[i];
+}
+return ans;
 
-//o(1) space, w/o per suf vectors
+//o(1) space, w/o prefix suffix vectors
+
 int n=nums.size();
 vector<int>ans(n,1);
 for(int i=1;i<n;i++){
