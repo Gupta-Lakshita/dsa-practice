9b54eff68cdc44be07c280a8015a00662bec5abe
Lakshita Gupta <me.lakshitag@gmailcom>
2026-06-11 15:06:18 +0530

    merge intervals

---
+++ arrays/Merge Overlapping Intervals.cpp
@@ -0,0 +1,19 @@
+//Merge Overlapping Intervals
+//approach: either directly adding to ans vector or accumulating in previous interval
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n= intervals.size();
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> ans;
+        for (int i=0;i<n;i++){
+            if(ans.empty() || intervals[i][0]>ans.back()[1]){
+                ans.push_back(intervals[i]);
+            }
+            else {
+                ans.back()[1]=max(ans.back()[1], intervals[i][1]);
+            }
+        }
+        return ans;
+    }
+};
