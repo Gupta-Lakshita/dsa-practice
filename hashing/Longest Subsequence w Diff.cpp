844479b9dc89f7794888162f74be980c81fedea1
Lakshita Gupta <me.lakshitag@gmail.com>
2026-06-16 23:20:18 +0530

    hashing

---
+++ hashing/Longest Subsequence w Diff.cpp
@@ -0,0 +1,37 @@
+//Longest Subsequence w Diff 0/1
+//approach: HashMap Dynamic Programming problem 
+//where values (not indices) are used as DP states
+
+int maxLenSub(vector<int>& arr)
+{
+    unordered_map<int, int> dp;
+    int ans = 0;
+
+    for (int x : arr)
+    {
+        int best = 0;
+
+        if (dp.count(x - 1))
+            best = max(best, dp[x - 1]);
+
+        if (dp.count(x))
+            best = max(best, dp[x]);
+
+        if (dp.count(x + 1))
+            best = max(best, dp[x + 1]);
+
+        dp[x] = best + 1;
+        ans = max(ans, dp[x]);
+    }
+
+    return ans;
+}
+
+int main()
+{
+    vector<int> arr = {2, 5, 6, 3, 7, 6, 5, 8};
+
+    cout << maxLenSub(arr);
+
+    return 0;
+}
